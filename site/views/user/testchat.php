fd730a4622e000b8c2b1b6289504323b670e41d9
Trung <nttrung211@gmail.com>
2018-05-21 10:10:07 +0700

    update chat page

--- site/views/user/testchat.php
+++ site/views/user/testchat.php
@@ -1,8 +1,3 @@
-<script>
-    $(document).ready(function () {
-        jqcc.cometchat.chatWithUID(126)
-    });
-</script>
 <section class="breadcrumb-custom">
     <div class="container">
         <div class="row">
@@ -18,7 +13,19 @@
         <div class="frm_resgister">
             <div class="row">
                 <div class="col-xs-12 col-sm-12">
-                    <div id="cometchat_embed_synergy_container" style="width:1100px;height:600px;max-width:100%;border:1px solid #CCCCCC;border-radius:5px;overflow:hidden;" ></div><script src="<?php echo base_url();?>cometchat/js.php?type=core&name=embedcode" type="text/javascript"></script><script>var iframeObj = {};iframeObj.module="synergy";iframeObj.style="min-height:420px;min-width:350px;";iframeObj.width="1100px";iframeObj.height="600px";iframeObj.src="<?php echo base_url();?>cometchat/cometchat_embedded.php"; if(typeof(addEmbedIframe)=="function"){addEmbedIframe(iframeObj);}</script>
+                    <div id="cometchat_embed_synergy_container" style="width:1100px;height:600px;max-width:100%;border:1px solid #CCCCCC;border-radius:5px;overflow:hidden;" ></div>
+                    <script src="<?php echo base_url();?>cometchat/js.php?type=core&name=embedcode" type="text/javascript"></script>
+                    <script>
+                        var iframeObj = {};
+                        iframeObj.module="synergy";
+                        iframeObj.style="min-height:420px;min-width:350px;";
+                        iframeObj.width="1100px";
+                        iframeObj.height="600px";
+                        iframeObj.src="<?php echo base_url();?>cometchat/cometchat_embedded.php";
+                        if(typeof(addEmbedIframe)=="function"){
+                            addEmbedIframe(iframeObj);
+                        }
+                    </script>
                 </div>
             </div>
         </div>
