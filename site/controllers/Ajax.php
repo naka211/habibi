e132f8aa5480fdba1c3cb1fa035328a425d28cf2
Trung <nttrung211@gmail.com>
2018-07-15 17:47:18 +0700

    change name in approval email

--- site/controllers/Ajax.php
+++ site/controllers/Ajax.php
@@ -544,7 +544,7 @@ class Ajax extends MX_Controller{
         $user = $this->session->userdata('user');
         $link = base_url().'admin/en/mod_images/images?name='.$user->name.'&status=0';
         $content = 'Hej Admin<br /><br />
-                        Joe har uploadet billede, se venligst dette link for at tjekke det: '.$link.'<br /><br />
+                        '.$user->name.' har uploadet billede, se venligst dette link for at tjekke det: '.$link.'<br /><br />
                         Med venlig hilsen<br/>
                         <a href="'.base_url().'">Zeduuce.com®</a>';
         $this->general_model->sendEmail(['trung@mywebcreations.dk'], 'Zeduuce.com - '.$user->name.'har uploadet billede', $content);
