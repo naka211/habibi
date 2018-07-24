5ed0631f5bf847d42bae6e64969c4c01fdc60e46
Trung <nttrung211@gmail.com>
2018-07-24 10:12:02 +0700

    change send email protocol

--- site/models/General_model.php
+++ site/models/General_model.php
@@ -60,13 +60,13 @@ class General_model extends CI_Model{
     function sendEmail($emails, $subject, $content, $data = array(), $from = null, $mailType = 'html')
     {
         $configEmail['mailtype'] = $mailType;
-        $configEmail['protocol'] = 'sendmail';
-        /*$configEmail['smtp_host'] = 'asmtp.unoeuro.com';
+        $configEmail['protocol'] = 'smtp';
+        $configEmail['smtp_host'] = 'asmtp.unoeuro.com';
         $configEmail['smtp_user'] = 'noreply@zeduuce.com';
-        $configEmail['smtp_pass'] = 'yi16wunegapu';
+        $configEmail['smtp_pass'] = 'mTXz6X=A3y5dE?T6';
         $configEmail['smtp_port'] = 587;
         $configEmail['smtp_crypto'] = 'tls';
-        $configEmail['smtp_timeout'] = 30;*/
+        $configEmail['smtp_timeout'] = 30;
 
         $this->load->library('email');
         $this->email->set_newline("\r\n");
