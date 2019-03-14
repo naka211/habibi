c6d1b14f3a9e2de55027cd9aeff3e225e0168008
Trung <nttrung211@gmail.com>
2019-03-14 13:36:14 +0700

    to debug

--- admin/modules/mod_user/controllers/User.php
+++ admin/modules/mod_user/controllers/User.php
@@ -13,8 +13,6 @@ class User extends CI_Controller{
     }
 
     function index($page=0){
-        var_dump(file_exists($this->config->item('root')."uploads/raw_thumb_user/9ed447485a4c92e8ed8e9c2da050c55a.JPG"));exit();
-        unlink($this->config->item('root')."uploads/raw_thumb_user/9ed447485a4c92e8ed8e9c2da050c55a.JPG");
         $this->check->check('view','','',base_url());
         if($this->check->check('add')){
             $data['add'] = $this->module_name.'/user/add';
