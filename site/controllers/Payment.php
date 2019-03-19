7d48d31b2072a73cc45c210933627b486d7530e9
Trung <nttrung211@gmail.com>
2019-03-19 10:56:24 +0700

    to check the recurring callback

--- site/controllers/Payment.php
+++ site/controllers/Payment.php
@@ -42,6 +42,69 @@ class Payment extends MX_Controller {
         $this->load->view('templates', $data);
     }
 
+    public function upgradeSuccess($userId){
+        $data = array();
+        $this->user->addMeta($this->_meta, $data);
+
+        $user = $this->user->getUser($userId);
+        if($user->package == 1){
+            $plusTime = '+1 month';
+        } else if($user->package == 3){
+            $plusTime = '+3 months';
+        } else if($user->package == 6){
+            $plusTime = '+6 months';
+        } else {
+            $plusTime = '+1 day';
+        }
+
+        $DB['type'] = 2;
+        $DB['paymenttime'] = time();
+        $DB['expired_at'] = strtotime($plusTime, $DB['paymenttime']);
+        $this->user->saveUser($DB, $userId);
+
+        $data['page'] = 'user/upgradeSuccess';
+        $this->load->view('templates', $data);
+    }
+
+    public function upgradeCancel(){
+        customRedirectWithMessage(site_url('user/index'), 'Din betaling mislykkedes');
+    }
+
+    public function upgradeCallback($userId){
+        $requestBody = file_get_contents("php://input");
+        $request = json_decode($requestBody);
+
+        // Check checksum
+        /*$key = '196543afab47e2f8552ee61d99f658562937118aaa709d458943e20c110764e3';
+        $checksum = hash_hmac("sha256", $requestBody, $key);
+        if ($checksum != $_SERVER["HTTP_QUICKPAY_CHECKSUM_SHA256"]) {
+            return null;
+        }*/
+
+        $user = $this->user->getUser($userId);
+        $operation = end($request->operations);
+        $metadata = $request->metadata;
+        //Update payment
+        $DB['price'] = $operation->amount/100;
+        $DB['subscriptionid'] = $request->id;
+        $DB['orderid'] = $request->order_id;
+        $DB['cardno']    = $metadata->bin.'XXXXXX'.$metadata->last4;
+        $this->user->saveUser($DB, $userId);
+        //Add to log
+        //$this->addPaymentLog($userId);
+
+        //Send email
+        $sendEmailInfo['name']      = $user->name;
+        $sendEmailInfo['email']     = $user->email;
+        $sendEmailInfo['orderId']   = $DB['orderid'];
+        $sendEmailInfo['price']     = $DB['price'].' DKK';
+        $sendEmailInfo['expired']   = date('d/m/Y', $DB['expired_at']);
+        $emailTo = array($user->email);
+        sendEmail($emailTo,'upgradeGoldMember',$sendEmailInfo,'');
+
+        return true;
+    }
+
     function changeCard(){
         //$userid = $this->session->userdata('userid');
         //Go payment Epay
@@ -60,12 +123,52 @@ class Payment extends MX_Controller {
         $this->load->view('templates', $data);
     }
 
+    public function changeCardSuccess(){
+        $user = $this->session->userdata('user');
+
+        //Update card info
+        $DB['subscriptionid'] = $this->input->get('subscriptionid');
+        $DB['cardno']    = $this->input->get('cardno');
+        $this->user->saveUser($DB, $user->id);
+
+        customRedirectWithMessage(site_url('user/update'), 'Ændring af kortoplysningerne');
+    }
+
+    public function changeCardCancel(){
+        customRedirectWithMessage(site_url('user/update'), 'Ændring af kortoplysningerne fejler');
+    }
+
+    public function changeCardCallback(){
+
+    }
+
+    public function addPaymentLog($userId){
+        if($this->input->get('txnid')){
+            $logDb['userId']    = $userId;
+            $logDb['txnid']     = $this->input->get('txnid');
+            $logDb['orderId']   = $this->input->get('orderid');
+            $logDb['amount']    = $this->input->get('amount')/100;
+            $logDb['currency']  = $this->input->get('currency');
+            $logDb['date']      = $this->input->get('date');
+            $logDb['time']      = $this->input->get('time');
+            $logDb['hash']      = $this->input->get('hash');
+            $logDb['txnfee']    = $this->input->get('txnfee');
+            $logDb['cardno']    = $this->input->get('cardno');
+            $id = $this->user->addLog($logDb);
+            if($id == false){
+                customRedirectWithMessage(site_url('user/index'), 'Fejl ved lagring af log');
+            }
+        } else {
+            customRedirectWithMessage(site_url('user/index'), 'Kan ikke finde betalingsoplysninger');
+        }
+    }
+
     public function getFee(){
         $users = $this->user->getExpiredUsers();
         if($users){
             foreach ($users as $user){
                 if($user->stand_by_payment != 2){
-                    $orderId = 'US-'.randomPassword();
+                    $orderId = randomPassword();
 
                     if($user->package == 1){
                         $packageName = 'price1Month';
@@ -146,122 +249,26 @@ class Payment extends MX_Controller {
         }
     }
 
-    public function upgradeSuccess($userId){
-        $data = array();
-        $this->user->addMeta($this->_meta, $data);
-
-        $user = $this->user->getUser($userId);
-        if($user->package == 1){
-            $plusTime = '+1 month';
-        } else if($user->package == 3){
-            $plusTime = '+3 months';
-        } else if($user->package == 6){
-            $plusTime = '+6 months';
-        } else {
-            $plusTime = '+1 day';
-        }
-
-        $DB['type'] = 2;
-        $DB['paymenttime'] = time();
-        $DB['expired_at'] = strtotime($plusTime, $DB['paymenttime']);
-        $this->user->saveUser($DB, $userId);
-
-        $data['page'] = 'user/upgradeSuccess';
-        $this->load->view('templates', $data);
-    }
-
-    public function upgradeCancel(){
-        customRedirectWithMessage(site_url('user/index'), 'Din betaling mislykkedes');
-    }
-
-    public function upgradeCallback($userId){
+    public function recurringCallback(){
         $requestBody = file_get_contents("php://input");
         $request = json_decode($requestBody);
-
-        // Check checksum
-        /*$key = '196543afab47e2f8552ee61d99f658562937118aaa709d458943e20c110764e3';
-        $checksum = hash_hmac("sha256", $requestBody, $key);
-        if ($checksum != $_SERVER["HTTP_QUICKPAY_CHECKSUM_SHA256"]) {
-            return null;
-        }*/
-
-        $user = $this->user->getUser($userId);
-        $operation = end($request->operations);
-        $metadata = $request->metadata;
-        //Update payment
-        $DB['price'] = $operation->amount/100;
-        $DB['subscriptionid'] = $request->id;
-        $DB['orderid'] = $request->order_id;
-        $DB['cardno']    = $metadata->bin.'XXXXXX'.$metadata->last4;
-        $this->user->saveUser($DB, $userId);
-        //Add to log
-        //$this->addPaymentLog($userId);
-
-        //Send email
-        $sendEmailInfo['name']      = $user->name;
-        $sendEmailInfo['email']     = $user->email;
-        $sendEmailInfo['orderId']   = $DB['orderid'];
-        $sendEmailInfo['price']     = $DB['price'].' DKK';
-        $sendEmailInfo['expired']   = date('d/m/Y', $DB['expired_at']);
-        $emailTo = array($user->email);
-        sendEmail($emailTo,'upgradeGoldMember',$sendEmailInfo,'');
-
-        return true;
-    }
-
-    public function changeCardSuccess(){
-        $user = $this->session->userdata('user');
-
-        //Update card info
-        $DB['subscriptionid'] = $this->input->get('subscriptionid');
-        $DB['cardno']    = $this->input->get('cardno');
-        $this->user->saveUser($DB, $user->id);
-
-        customRedirectWithMessage(site_url('user/update'), 'Ændring af kortoplysningerne');
-    }
-
-    public function changeCardCancel(){
-        customRedirectWithMessage(site_url('user/update'), 'Ændring af kortoplysningerne fejler');
-    }
-
-    public function changeCardCallback(){
-
-    }
-
-    public function addPaymentLog($userId){
-        if($this->input->get('txnid')){
-            $logDb['userId']    = $userId;
-            $logDb['txnid']     = $this->input->get('txnid');
-            $logDb['orderId']   = $this->input->get('orderid');
-            $logDb['amount']    = $this->input->get('amount')/100;
-            $logDb['currency']  = $this->input->get('currency');
-            $logDb['date']      = $this->input->get('date');
-            $logDb['time']      = $this->input->get('time');
-            $logDb['hash']      = $this->input->get('hash');
-            $logDb['txnfee']    = $this->input->get('txnfee');
-            $logDb['cardno']    = $this->input->get('cardno');
-            $id = $this->user->addLog($logDb);
-            if($id == false){
-                customRedirectWithMessage(site_url('user/index'), 'Fejl ved lagring af log');
-            }
-        } else {
-            customRedirectWithMessage(site_url('user/index'), 'Kan ikke finde betalingsoplysninger');
-        }
+        log_message('debug', $request);
     }
 
     public function testRecurring(){
         $order_id = randomPassword();
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, 'https://api.quickpay.net/subscriptions/142737268/recurring');
+        curl_setopt($ch, CURLOPT_URL, 'https://api.quickpay.net/subscriptions/142737269/recurring');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"amount\":9900,\"order_id\":\"".$order_id."\"}");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"amount\":9900,\"order_id\":\"".$order_id."\", \"auto_capture\":\"true\"}");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_USERPWD, '' . ':' . 'c9150af0dff909ed414cc9373239be48288b3e4aabe8b60d7674a2832159d45f');
 
         $headers = array();
         $headers[] = 'Content-Type: application/json';
         $headers[] = 'Accept-Version: v10';
+        $headers[] = 'QuickPay-Callback-Url: '.site_url('payment/recurringCallback');
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
         $result = curl_exec($ch);print_r($result);exit();
