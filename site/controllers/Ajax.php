a14689011689f1e6a10ab5ea4bc963b39f4ce205
Trung <nttrung211@gmail.com>
2019-04-03 15:00:34 +0700

    to fix the image orientation

--- site/controllers/Ajax.php
+++ site/controllers/Ajax.php
@@ -502,6 +502,8 @@ class Ajax extends MX_Controller{
                 exit();
             } else {
                 $data = $this->upload->data();
+                //Correct the image orientation
+                $this->correctImageOrientation($data['full_path']);
                 //save to db
                 $DB['userId'] = $user->id;
                 $DB['image'] = $data['file_name'];
@@ -585,6 +587,8 @@ class Ajax extends MX_Controller{
                 exit();
             } else {
                 $data = $this->upload->data();
+                //Correct the image orientation
+                $this->correctImageOrientation($data['full_path']);
                 //save to db
                 $newAvatar = $this->user->getNewAvatar($user->id);
                 if($newAvatar != ''){
@@ -763,6 +767,35 @@ class Ajax extends MX_Controller{
         header('Content-Type: application/json');
         echo json_encode($data);
         return;
+    }
+
+    function correctImageOrientation($filename) {
+        if (function_exists('exif_read_data')) {
+            $exif = exif_read_data($filename);
+            if($exif && isset($exif['Orientation'])) {
+                $orientation = $exif['Orientation'];
+                if($orientation != 1){
+                    $img = imagecreatefromjpeg($filename);
+                    $deg = 0;
+                    switch ($orientation) {
+                        case 3:
+                            $deg = 180;
+                            break;
+                        case 6:
+                            $deg = 270;
+                            break;
+                        case 8:
+                            $deg = 90;
+                            break;
+                    }
+                    if ($deg) {
+                        $img = imagerotate($img, $deg, 0);
+                    }
+                    // then rewrite the rotated image back to the disk as $filename
+                    imagejpeg($img, $filename, 95);
+                } // if there is some rotation necessary
+            } // if have the exif orientation info
+        } // if function exists
     }
 }
 ?>
