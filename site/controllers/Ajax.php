e5bfc7b6c4582b5a9b3f1bf4c8023823a09f515e
Trung <nttrung211@yahoo.com>
2020-02-11 00:29:52 +0700

    to fix the correctImageOrientation function

--- site/controllers/Ajax.php
+++ site/controllers/Ajax.php
@@ -1035,14 +1035,13 @@ class Ajax extends MX_Controller{
         return;
     }
 
-    function correctImageOrientation($filename, $extension = 'png') {
+    /*function correctImageOrientation($filename, $extension = 'png') {
         if (function_exists('exif_read_data')) {
             $exif = exif_read_data($filename);
             if($exif && isset($exif['Orientation'])) {
                 $orientation = $exif['Orientation'];
                 if($orientation != 1){
                     if($extension == 'jpg'){
-                        header('Content-Type: image/jpeg');
                         $img = imagecreatefromjpeg($filename);
                     } else {
                         $img = imagecreatefrompng($filename);
@@ -1069,6 +1068,35 @@ class Ajax extends MX_Controller{
                         imagepng($img, $filename, 95);
                     }
                     imagedestroy($img);
+                } // if there is some rotation necessary
+            } // if have the exif orientation info
+        } // if function exists
+    }*/
+
+    function correctImageOrientation($filename) {
+        if (function_exists('exif_read_data')) {
+            $exif = exif_read_data($filename);
+            if($exif && isset($exif['Orientation'])) {
+                $orientation = $exif['Orientation'];
+                if($orientation != 1){
+                    $img = imagecreatefromjpeg($filename);
+                    $deg = 0;
+                    switch ($orientation) {
+                        case 3:
+                            $deg = 180;
+                            break;
+                        case 6:
+                            $deg = 270;
+                            break;
+                        case 8:
+                            $deg = 90;
+                            break;
+                    }
+                    if ($deg) {
+                        $img = imagerotate($img, $deg, 0);
+                    }
+                    // then rewrite the rotated image back to the disk as $filename
+                    imagejpeg($img, $filename, 95);
                 } // if there is some rotation necessary
             } // if have the exif orientation info
         } // if function exists
